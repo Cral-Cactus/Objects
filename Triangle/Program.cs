88556f4ca98f420f573ad645e02f6d6e157748a2
Cral_Cactus <marian.pb@abv.bg>
2023-04-27 14:20:25 +0300

    Creation of Triangle

---
+++ Triangle/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Triangle
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+
+            string[] parts = input.Split(' ');
+            double a, h;
+
+            if (!double.TryParse(parts[0], out a))
+            {
+                Console.WriteLine("Invalid input for a. Please enter a valid number.");
+                return;
+            }
+
+            if (!double.TryParse(parts[1], out h))
+            {
+                Console.WriteLine("Invalid input for h. Please enter a valid number.");
+                return;
+            }
+
+            Triangle t = new Triangle(a, h);
+
+            Console.WriteLine($"Triangle with side {t.A} and height to it {t.H}");
+            Console.WriteLine($"Triangle face: {t.GetArea()}");
+
+            t.PrintTriangleInfo();
+        }
+    }
+}
---
+++ Triangle/Triangle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Triangle
+{
+    public class Triangle
+    {
+        private double a;
+        private double h;
+
+        public double A
+        {
+            get { return a; }
+            set { a = value; }
+        }
+
+        public double H
+        {
+            get { return h; }
+            set { h = value; }
+        }
+
+        public Triangle(double a, double h)
+        {
+            this.a = a;
+            this.h = h;
+        }
+
+        public double GetArea()
+        {
+            //return 0.5 * a * h;
+            return a * h / 2;
+        }
+
+        public void PrintTriangleInfo()
+        {
+            Console.WriteLine($"SIDE = {this.A} and HEIGHT to IT = {this.H} with FACE = {GetArea()}");
+        }
+    }
+}
