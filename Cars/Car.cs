985f67457987c8ee99a2fd9b577fea9be5c6813f
Cral_Cactus <marian.pb@abv.bg>
2023-05-10 14:48:34 +0300

    Creation of Cars

---
+++ Cars/Car.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Cars
+{
+    internal class Car
+    {
+        private string brand;
+        private string model;
+        private string color;
+        private string carPlate;
+        private int productionYear;
+        private static int count;
+
+        string eco;
+
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public string CarPlate { get; set; }
+        public int ProductionYear
+        {
+            get
+            {
+                return this.productionYear;
+            }
+            set
+            {
+                if (value > 2000)
+                {
+                    eco = "ECO";
+                }
+                else
+                {
+                    eco = "I'm NOT Eco CAR";
+                }
+
+                this.productionYear = value;
+            }
+        }
+        public static int Count
+        {
+            get { return count; }
+        }
+
+        public Car(string brand, string model, string color, string carPlate, int productionYear)
+        {
+            this.Brand = brand;
+            this.Model = model;
+            this.Color = color;
+            this.CarPlate = carPlate;
+            this.ProductionYear = productionYear;
+            count++;
+        }
+
+        public void Move()
+        {
+            Console.WriteLine($"{this.Brand} {this.Model}: Brym - brum - brum");
+        }
+
+        public void CheckEcological()
+        {
+            Console.WriteLine(eco);
+        }
+
+        public void Present()
+        {
+            Console.WriteLine($"Brand: {this.Brand} | Model: {this.Model} | Color: {this.Color} | Car Plate: {this.CarPlate} | Production Year: {this.ProductionYear}");
+        }
+    }
+}
---
+++ Cars/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Cars
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            List<Car> cars = new List<Car>();
+            string input;
+
+            Console.WriteLine("Enter car details (brand, model, color, license plate, year) separated by commas:\nend=END\n");
+
+            do
+            {
+                input = Console.ReadLine();
+
+                if (input != "end")
+                {
+                    string[] carDetails = input.Split(',');
+
+                    if (carDetails.Length == 5)
+                    {
+                        string brand = carDetails[0].Trim();
+                        string model = carDetails[1].Trim();
+                        string color = carDetails[2].Trim();
+                        string licensePlate = carDetails[3].Trim();
+                        int year;
+
+                        if (int.TryParse(carDetails[4].Trim(), out year))
+                        {
+                            Car car = new Car(brand, model, color, licensePlate, year);
+                            cars.Add(car);
+
+                            Console.WriteLine("Car added successfully.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid year. Please try again.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input. Please provide all car details.");
+                    }
+                }
+
+            } while (input != "end");
+
+            Console.WriteLine($"Total cars created: {Car.Count} / {cars.Count}");
+
+            foreach (Car car in cars)
+            {
+                car.Present();
+                car.Move();
+                car.CheckEcological();
+            }
+        }
+    }
+}
