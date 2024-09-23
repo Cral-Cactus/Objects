b3f4acce825312f0542b2f0673ae6425eae09ad3
Cral_Cactus <marian.pb@abv.bg>
2024-09-23 12:42:23 +0300

    Created Cats Homework

---
+++ Cats/Cat.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Cats
+{
+    public class Cat
+    {
+        private string name;
+        private int age;
+        private CatBreed breed;
+        private string color;
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public int Age
+        {
+            get { return this.age; }
+            set { this.age = value; }
+        }
+
+        public CatBreed Breed
+        {
+            get { return this.breed; }
+            set { this.breed = value; }
+        }
+
+        public string Color
+        {
+            get { return this.color; }
+            set { this.color = value; }
+        }
+
+        public Cat(string name, int age, CatBreed breed, string color)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Breed = breed;
+            this.Color = color;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.name} is {this.age} years old {this.color} {this.breed} cat.";
+        }
+    }
+
+    public enum CatBreed
+    {
+        Street,
+        Siamese,
+        Persian,
+        MaineCoon
+    }
+}
---
+++ Cats/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Cats
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            List<Cat> cats = new List<Cat>();
+            bool active = true;
+
+            while (active)
+            {
+                Console.WriteLine("Menu:");
+                Console.WriteLine("1: add cat");
+                Console.WriteLine("2: print all cats");
+                Console.WriteLine("3: print specific cat");
+                Console.WriteLine("4: exit");
+
+                string choice = Console.ReadLine();
+                Console.Clear();
+
+                switch (choice)
+                {
+                    case "1":
+                        AddCat(cats);
+                        break;
+
+                    case "2":
+                        PrintAllCats(cats);
+                        break;
+
+                    case "3":
+                        PrintSpecificCat(cats);
+                        break;
+
+                    case "4":
+                        active = false;
+                        Console.WriteLine("bye");
+                        break;
+
+                    default:
+                        Console.WriteLine("invalid choice");
+                        break;
+                }
+            }
+        }
+
+        static void AddCat(List<Cat> cats)
+        {
+            Console.Write("name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("age: ");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("breed (0: Street, 1: Siamese, 2: Persian, 3: MaineCoon): ");
+            int breedChoice = int.Parse(Console.ReadLine());
+            CatBreed breed = (CatBreed)breedChoice;
+
+            Console.Write("color: ");
+            string color = Console.ReadLine();
+
+            Cat newCat = new Cat(name, age, breed, color);
+            cats.Add(newCat);
+
+            Console.WriteLine("the cat was added!");
+        }
+
+        static void PrintAllCats(List<Cat> cats)
+        {
+            if (cats.Count > 0)
+            {
+                Console.WriteLine("all cats:");
+                foreach (Cat cat in cats)
+                {
+                    Console.WriteLine(cat.ToString());
+                }
+            }
+            else
+            {
+                Console.WriteLine("no cats");
+            }
+        }
+
+        static void PrintSpecificCat(List<Cat> cats)
+        {
+            if (cats.Count > 0)
+            {
+                Console.WriteLine($"cat id (1 to {cats.Count}):");
+                int catIndex = int.Parse(Console.ReadLine());
+
+                if (catIndex > 0 && catIndex <= cats.Count)
+                {
+                    Console.WriteLine(cats[catIndex - 1].ToString());
+                }
+                else
+                {
+                    Console.WriteLine("wrong id");
+                }
+            }
+            else
+            {
+                Console.WriteLine("no cats");
+            }
+        }
+    }
+}
